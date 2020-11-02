1350fbb6e5713186c3836a1357198da6819b00e9
fixiabis <fixiabis@gmail.com>
2020-11-03 00:26:52 +0800

    feat: add board to play page

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
+import { RelatiBoard } from '../components';
+import { RelatiGame } from '../libs';
+import { RelatiGrid } from '../libs/RelatiGame';
+
+const boardContainerStyle = {
+  width: '100%',
+  height: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
 
 const Play = () => {
-  return <>Play</>;
+  const game = useMemo(() => new RelatiGame(9, 9), []);
+  const [, setGameTurn] = useState(0);
+
+  const handleGridClick = ({ x, y }: RelatiGrid) => {
+    game.placePiece(x, y);
+    setGameTurn(game.turn);
+  };
+
+  return (
+    <div style={boardContainerStyle}>
+      <RelatiBoard value={game.board} onGridClick={handleGridClick} />
+    </div>
+  );
 };
 
 export default Play;
