ab0d55dd57598459a208e394b00d9a28650483dc
fixiabis <fixiabis@gmail.com>
2020-11-12 00:13:28 +0800

    fix: change router path

--- src/legacy/page-components/choose-mode/ChooseModeForGame.tsx
+++ src/legacy/page-components/choose-mode/ChooseModeForGame.tsx
@@ -11,10 +11,10 @@ const ChooseModeForGame: ChooseModePageComponent = ({ router, leavePage }) => {
   const toPlayPage1pWithPlayerO = () => toPlayPageWithPlayer('o');
   const toPlayPage1pWithPlayerX = () => toPlayPageWithPlayer('x');
   const toPlayPageWithPlayer = (player: string) =>
-    router.replace(`/play?1p&on=${size}&with=${player}`);
+    router.replace(`/legacy/play?1p&on=${size}&with=${player}`);
   const toPlayPage2p = () =>
     router.replace(
-      `${!navigator.onLine ? `/play?2p&` : `/wait-for-opponent?`}on=${size}`
+      `${!navigator.onLine ? `/legacy/play?2p&` : `/legacy/wait-for-opponent?`}on=${size}`
     );
 
   if (!size) {
